291d18a8bfc5307a49fb3152055fe57314fd7622
Muhammad Rajab Raza <mohammadrajabraza@gmail.com>
2021-11-22 22:37:49 +0500

    feat: add icon styles for large button variants

--- src/components/StyledButton/StyledButton.jsx
+++ src/components/StyledButton/StyledButton.jsx
@@ -1,9 +1,10 @@
 import { Button } from './styled'
+import React from 'react'
 const StyledButton = (props) => {
   const { text, children } = props
   return (
     <Button {...props} hasChildren={children && true}>
-      {children}
+      {children && React.cloneElement(children, { ...props })}
       {text}
     </Button>
   )
--- src/components/StyledIcons/index.js
+++ src/components/StyledIcons/index.js
@@ -6,4 +6,10 @@ export const SearchIcon = styled(Search)`
     height: 22px;
     width: 22px;
     margin-right: 4px;
+
+    ${props => props.large && `
+        height: 28px;
+        width: 28px;
+        margin-right: 8px;
+    `}
 `
